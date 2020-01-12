21eabfbfe0c559421964a8f0801438c3de3b6998
Jay <4a490063@stust.edu.tw>
2020-01-12 14:25:51 +0800

    Modify: Browserslist.

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  plugins: [
-    require('autoprefixer')
-  ]
-};
+  plugins: {
+    'autoprefixer': {
+      overrideBrowserslist: [
+        "Android 4.1",
+        "iOS 7.1",
+        "Chrome > 31",
+        "ff > 31",
+        "ie >= 8"
+      ]
+    },
+  }
+}
